857c994c16d30497715c44f22dc3644ce53fa2fe
Damian Krawczun <dkrawczun@lbpro.pl>
2022-11-09 15:36:24 +0100

    .

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -2,7 +2,40 @@ const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB Connection Successful');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour name is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Tour price is required'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
